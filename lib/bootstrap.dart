677ab091f2428774b7a035d171f999b6aebdd69a
Renan araujo <renanmatheuscorreiaaraujo@gmail.com>
2024-05-17 19:35:32 +0100

    wasm

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -6,8 +6,8 @@ import 'package:crystal_ball/gen/assets.gen.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 class AppBlocObserver extends BlocObserver {
   @override
@@ -33,13 +33,6 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   };
 
   WidgetsFlutterBinding.ensureInitialized();
-  final storage = await HydratedStorage.build(
-    storageDirectory: kIsWeb
-        ? HydratedStorage.webStorageDirectory
-        : await getApplicationDocumentsDirectory(),
-  );
-
-  HydratedBloc.storage = storage;
 
   Bloc.observer = AppBlocObserver();
 
